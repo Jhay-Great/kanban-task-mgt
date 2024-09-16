50998324bac0d1f7cf517a57db9ab2dae397557c
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-16 17:18:43 +0000

    adds initial files and folder structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideRouterStore(), provideEffects(), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
