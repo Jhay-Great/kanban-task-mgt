7d79798d3bcaf68f252483f0c7bb6a1d0c80987f
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-17 09:37:46 +0000

    adds comments

--- src/app/state/board/board.reducer.ts
+++ src/app/state/board/board.reducer.ts
@@ -2,6 +2,21 @@ import { createReducer, on } from "@ngrx/store";
 import { IBoard } from "../../model/board.interface";
 import { onLoadBoardData, onLoadBoardSuccess } from "./board.action";
 
+// entities
+/**
+ createEntityAdapter is part of the ngrx entity package that helps you create an adapter. 
+ adapters are methods that are used to manage your entity collections.
+ addOne, removeOne, are examples of some adapters
+ *  */
+import { createEntityAdapter } from "@ngrx/entity";
+
+/**
+ * EntityState is an interface provided by ngrx entities to used to define the structure of your data
+ * It provides you with an object with properties : id, entities
+ */
+import { EntityState } from "@ngrx/entity";
+
+
 const initialValue:IBoard[] = []
 
 // const initialValue:IBoard[] = {
