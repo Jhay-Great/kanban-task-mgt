c85546eeea90458b69e96bd23ffa587f3171e0ec
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-17 18:59:59 +0000

    add styles to sidebar

--- src/app/components/board-list/board-list.component.ts
+++ src/app/components/board-list/board-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppState } from '../../model/AppState';
+import { Store } from '@ngrx/store';
+import { selectBoard } from '../../state/board/board.selector';
+import { Observable } from 'rxjs';
+import { IBoard } from '../../model/board.interface';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-board-list',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './board-list.component.html',
   styleUrl: './board-list.component.scss'
 })
-export class BoardListComponent {
+export class BoardListComponent implements OnInit {
+
+  boards$!: Observable<IBoard[]>
+
+  constructor (
+    private store: Store<AppState>
+  ) {};
+
+  ngOnInit(): void {
+    this.boards$ = this.store.select(selectBoard);
+  }
 
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BoardListComponent } from '../board-list/board-list.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [BoardListComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
