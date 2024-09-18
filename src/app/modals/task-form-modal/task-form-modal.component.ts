e0e1d4b015bcf8259e55357a52a2b13a2ceacfb7
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-18 18:37:56 +0000

    feat: adds initial form builder implementation on init

--- src/app/modals/task-form-modal/task-form-modal.component.ts
+++ src/app/modals/task-form-modal/task-form-modal.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-form-modal.component.html',
   styleUrl: './task-form-modal.component.scss'
 })
-export class TaskFormModalComponent {
+export class TaskFormModalComponent implements OnInit, OnDestroy {
+
+  taskForm!: FormGroup;
+
+  constructor (
+    private fb: FormBuilder,
+  ) {};
+
+  ngOnInit(): void {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      subtask: this.fb.array([]),
+      status: this.fb.array([]),
+    })
+  }
+
+  ngOnDestroy(): void {
+    
+  }
 
 }
