db0d2ea1b4d2cc42a0c08ba5425aebfa3dedcfa6
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-18 00:38:30 +0000

    adds form methods

--- src/app/components/board-list/board-list.component.ts
+++ src/app/components/board-list/board-list.component.ts
@@ -6,6 +6,8 @@ import { Observable } from 'rxjs';
 import { IBoard } from '../../model/board.interface';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { createBoard } from '../../state/board/board.action';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-board-list',
@@ -17,13 +19,61 @@ import { RouterLink } from '@angular/router';
 export class BoardListComponent implements OnInit {
 
   boards$!: Observable<IBoard[]>
+  form!: FormGroup
 
   constructor (
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private fb: FormBuilder,
   ) {};
 
   ngOnInit(): void {
+    // selects board array
     this.boards$ = this.store.select(selectBoard);
+
+    // creates form
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      columns: this.fb.array([]),
+    })
+  }
+
+  get columnFormArray () {
+    return this.form.get('columns') as FormArray;
+  }
+
+  createNewColumns () {
+    return this.fb.group({
+      columns: ['', Validators.required],
+    })
+  }
+  
+  addColumns () {
+    this.columnFormArray.push(this.createNewColumns());
+  }
+
+  removeColumn (index:number) {
+    this.columnFormArray.removeAt(index);
+  }
+
+  submit() {
+    const board = this.form.value;
+    this.store.dispatch(createBoard({board}));
+  }
+
+  createNewBoard () {
+    // console.log('called...')
+    // this.store.dispatch(createBoard)
+    //     const data = {
+    //   name: 'boy',
+    //   id: '39sskls3',
+    //   columns: [
+    //     {
+    //       name: 'going out',
+    //       tasks: [],
+    //     }
+    //   ]
+    // }
+    // this.store.dispatch(createBoard({board: data}));
   }
 
 }
