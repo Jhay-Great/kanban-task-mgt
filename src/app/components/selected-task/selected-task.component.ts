28e2a575bddb85b425606533cf89222936c4391d
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-20 09:11:58 +0000

    feat: adds initial logic to read completed subtasks dynamically

--- src/app/components/selected-task/selected-task.component.ts
+++ src/app/components/selected-task/selected-task.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApplicationService } from '../../services/application/application.service';
-import { Observable } from 'rxjs';
+import { filter, map, Observable, tap } from 'rxjs';
 import { IColumns, ITask } from '../../model/board.interface';
 import { AsyncPipe } from '@angular/common';
 
@@ -15,6 +15,7 @@ export class SelectedTaskComponent implements OnInit {
   title:string = 'hello'
   task!:Observable<ITask | null>;
   columns$!:Observable<IColumns[]>;
+  statusCount!:Observable<number>;
 
   constructor (
     private appService: ApplicationService,
@@ -26,6 +27,44 @@ export class SelectedTaskComponent implements OnInit {
     this.task = data;
 
     this.columns$ = this.appService.getColumns();
+
+    // checking completed task
+    // this.statusCount = this.task.pipe(
+    //   filter(data => data !== null),
+    //   map(task =>
+    //     task?.subtasks.map(subtask => {
+    //       let count = 0;
+    //       subtask.isComplete && count++;
+    //       return count
+    //     }
+    //     )
+    //   ),
+    //   tap(data => console.log(data))
+    // )
+    // this.statusCount = this.task.pipe(
+    //   filter(data => data !== null),
+    //   map(task => 
+    //     task?.subtasks.reduce((count, subtask) => {
+    //       return subtask.isComplete ? count + 1 : count;
+    //     }, 0) // Start counting from 0
+    //   ),
+    //   tap(data => console.log(data)) // Log the total count
+    // )
+    
+    this.statusCount = this.task.pipe(
+      filter(data => data !== null),  // Ensure that the task is not null
+      map(task => {
+        // Ensure subtasks exist, otherwise return 0
+        return task?.subtasks
+          ? task.subtasks.reduce((count, subtask) => {
+              return subtask.isComplete ? count + 1 : count;
+            }, 0)  // Start counting from 0
+          : 0;  // Return 0 if task or subtasks are undefined
+      }),
+      tap(data => console.log(data))  // Log the total count
+    )
+    
+    
 
 
   }
